bfa97d5e0334d1b5479d7679b3ee704950d6ffa1
hubson123 <hubert.firek@o2.pl>
2024-04-03 21:48:37 +0200

    POK-2: add styles to home page

--- src/components/HomePage/Home.styled.tsx
+++ src/components/HomePage/Home.styled.tsx
@@ -1,12 +1,49 @@
 'use client'
 import styled from "styled-components";
 import { Flex } from "../Flex.styled";
-import { SPACES } from "@/types/constants";
+import { COLORS, SPACES } from "@/types/constants";
+import { Button } from "../Button.styled";
 
 export const HomeContainer = styled(Flex)`
+    background-color: ${COLORS.lightGray};
     flex-direction: column;
 `
-
-export const ListItem = styled(Flex)`
-    padding: ${SPACES.xs}px;
+export const HiddenButton = styled(Button)`
+    display: none;
 `
+export const ListItem = styled.div<{url:string}>`
+    padding: ${SPACES.xs}px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-color: wheat;
+    background-image: url(${props => props.url});
+    background-position-y: ${SPACES.m}px;
+    width: 200px;
+    height: 200px;
+    cursor: pointer;
+    h3{
+        color: darkBlue;
+        padding: 0;
+        margin-top: -${SPACES.xxs}px;
+        background-color: orange;
+        text-transform: capitalize;
+    }
+    &:hover{
+        opacity: .7;
+        background-color: rgb(180, 180, 184);
+            ${HiddenButton}{
+            display: block;
+            opacity: 1;
+            margin: 60px auto 0 auto;
+        }}
+`
+export const ListWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    flex-wrap: wrap;
+    max-width: 1200px;
+    gap: ${SPACES.m}px;
+`
+
--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,6 +1,18 @@
 
 export const API_URL = 'https://pokeapi.co/api/v2/'
+
+export const COLORS = {
+  blue: '#4FC1E9',
+  green: '#77CC55',
+  lightGray: '#F5F5F5',
+  gray: 'gray',
+  orange: '#FFA500',
+  yellow: '#FFCB05'
+
+}
+
 export const IMAGE_PATH = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
 export const SPACES = {
     xxs: 2,
     xs: 4,
